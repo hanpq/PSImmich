8525a37887a89f6022aa8382bc2719d8cc70fec3
Hannes Palmquist <hannes.palmquist@outlook.com>
2024-10-15 22:33:36 +0200

    new function to allow mocking of validatetoken

---
+++ source/Private/ValidateToken.ps1
@@ -0,0 +1,34 @@
+﻿function ValidateToken
+{
+    <#
+    .DESCRIPTION
+        Helper function to validate immich token
+    .PARAMETER Type
+        Defines the type of authentication to be performed, credential or accesstoken
+    .PARAMETER APIUrl
+        Defines the immich api url
+    .PARAMETER Secret
+        Defines the secret to use.
+    .EXAMPLE
+        ValidateToken -Type 'AccessToken' -ApiURL 'https://immich.contoso.com/api' -Secret (ConvertTo-SecureString -String 'token' -AsPlainText -Force)
+    #>
+    param (
+        $Type,
+        $APIUrl,
+        $Secret
+    )
+
+    switch ($Type)
+    {
+        'Credential'
+        {
+            $Result = Invoke-RestMethod -Method Post -Uri "$($ApiUrl)/auth/validateToken" -Headers @{Authorization = "Bearer $(ConvertFromSecureString -SecureString $Secret)" } | Select-Object -Property AuthStatus
+            return $Result
+        }
+        'AccessToken'
+        {
+            $Result = Invoke-RestMethod -Method Post -Uri "$($ApiUrl)/auth/validateToken" -Headers @{'X-API-Key' = "$(ConvertFromSecureString -SecureString $Secret)" } | Select-Object -Property AuthStatus
+            return $Result
+        }
+    }
+}
