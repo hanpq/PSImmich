c58660a530ed9c3b2babec78af2ddddf2f4a507d
Hannes Palmquist <hannes.palmquist@outlook.com>
2025-11-06 21:14:46 +0100

    Added ApiParameter class

---
+++ source/Classes/ApiParameterAttribute.class.ps1
@@ -0,0 +1,13 @@
+﻿class ApiParameterAttribute : System.Attribute
+{
+    [string]$Name
+
+    ApiParameterAttribute([string]$name)
+    {
+        $this.Name = $name
+    }
+}
+
+# Create a type accelerator for the shortened syntax [ApiParameter]
+$TypeAccelerators = [psobject].Assembly.GetType('System.Management.Automation.TypeAccelerators')
+$TypeAccelerators::Add('ApiParameter', [ApiParameterAttribute])
